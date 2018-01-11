190104b7ee4484f10abcc86588f564427183a0cd
namostute <namostute@namostute-Inspiron-N5050>
2018-01-11 20:27:36 +0530

    new code

---
+++ corejava/src/com/java/ReflectionDemo.java
@@ -0,0 +1,38 @@
+package com.java;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+class Student1{
+	private String name;
+	private int rollno;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	
+}
+public class ReflectionDemo {
+	public static void main(String[] args) {
+		Student1 s = new Student1();
+		Class c	= s.getClass();
+		System.out.println(c.getName());
+		Method [] m = c.getDeclaredMethods();
+		for(int i=0;i<m.length;i++){
+			System.out.println(m[i]);
+		}
+		Field[] f = c.getDeclaredFields();
+		for(int i=0;i<f.length;i++){
+			System.out.println(f[i]);
+		}
+	}
+
+}
