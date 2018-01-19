453ce6b9a20db0203c90a23ca7dc477e9c4b11dc
namostute <namostute@namostute-Inspiron-N5050>
2018-01-19 23:36:51 +0530

    new code

--- corejava/src/com/stringquestion/ReadFileDemo.java
+++ corejava/src/com/stringquestion/ReadFileDemo.java
@@ -4,38 +4,26 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.StringTokenizer;
 
 public class ReadFileDemo {
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {
-		Map<String, Integer>map = new HashMap<>();
-		@SuppressWarnings("resource")
 		BufferedReader bf = new BufferedReader(new FileReader("dk.txt"));
+		Map<String, Integer> map = new HashMap<>();
+		String[] words;
 		String string = bf.readLine();
-		while(string !=null){
-			StringTokenizer st = new StringTokenizer(string, " ");
-			while(st.hasMoreTokens()){
-				String temp = st.nextToken().toLowerCase();
-				if(map.containsKey(temp)){
-					map.put(temp, map.get(temp)+1);
+		while (string != null) {
+			words = string.split(" ");
+			for (String word : words) {
+				if (map.containsKey(word)) {
+					map.put(word, (map.get(word) + 1));
+				} else {
+					map.put(word, 1);
 				}
-				else
-					map.put(temp, 1);
 			}
 			string = bf.readLine();
-			Set s = map.entrySet();
-			Iterator it = s.iterator();
-			while(it.hasNext()){
-				Map.Entry<String, Integer> entry = (Entry<String, Integer>) it.next();
-				System.out.println(entry.getKey()+"\t"+entry.getValue());
-			}
 		}
-		
+		System.out.println(map);
 	}
-
 }
