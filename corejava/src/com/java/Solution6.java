f3464f88f0a9f76781afedb691a6d0c6b712285c
namostute <namostute@namostute-Inspiron-N5050>
2018-04-04 11:35:59 +0530

    new code
    Change-Id: I2de2c9e8db34ca4d62079d0430611aff5d90ef0c

---
+++ corejava/src/com/java/Solution6.java
@@ -0,0 +1,33 @@
+package com.java;
+
+public class Solution6 {
+	public static int difference(String[]pro,double[]price,String[]pro1,double[]price1){
+		int count = -1;
+		for(int i=0;i<pro.length-1;i++){
+			for(int j=0;j<pro1.length-1;j++){
+				if(pro[i]==pro1[j]){
+					for(int k=0;k<price.length-1;k++){
+						for(int l=0;l<price1.length-1;l++){
+							if(price[k] !=price1[l]){
+								count++;
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+		return count;
+		
+	}
+	public static void main(String[] args) {
+		String[]pro ={"banana","mango","apple","ghee"};
+		double[]price={12.50,24.45,12.60,450.50};
+		String[]pro1={"mango","ghee"};
+		double[]price1={25.67,500.54};
+		int c = difference(pro, price, pro1, price1);
+		System.out.println(c);
+	}
+	
+
+}
