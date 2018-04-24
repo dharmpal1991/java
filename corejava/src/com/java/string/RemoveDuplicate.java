f91f85ef0e0abdaffd8b904ef5ce3298609f3663
namostute <namostute@namostute-Inspiron-N5050>
2018-04-24 11:39:01 +0530

    new code

--- corejava/src/com/java/string/RemoveDuplicate.java
+++ corejava/src/com/java/string/RemoveDuplicate.java
@@ -1,6 +1,7 @@
 package com.java.string;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -8,7 +9,11 @@ public class RemoveDuplicate {
 	public static void removeDuplicateInString(String str){
 		String [] s = str.split(" ");
 		Set<String> set = new LinkedHashSet<>(Arrays.asList(s));
-		System.out.print(set);
+		Iterator<String> it = set.iterator();
+		while(it.hasNext()){
+			String string = it.next();
+			System.out.print(string+" ");
+		}
 	}
 	public static void main(String[] args) {
 		String s = "This is a book but not a noval its only book";
